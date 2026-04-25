33f182c086590b398f4460e09e3f2c7a7612dfb2
Isra23343045 <alfattah.isra@gmail.com>
2026-04-25 11:19:40 +0700

    add language selection

--- src/components/ContactApp.jsx
+++ src/components/ContactApp.jsx
@@ -6,6 +6,7 @@ import AddPage from "../pages/AddPage";
 import RegisterPage from "../pages/RegisterPage";
 import LoginPage from "../pages/LoginPage";
 import { getUserLogged, putAccessToken } from "../utils/api";
+import { LocaleProvider } from "../contexts/LocaleContext";
 
 class ContactApp extends React.Component {
   constructor(props) {
@@ -14,6 +15,21 @@ class ContactApp extends React.Component {
     this.state = {
       authedUser: null,
       initializing: true,
+      localeContext: {
+        locale: localStorage.getItem("locale") || "id",
+        toggleLocale: () => {
+          this.setState((prevState) => {
+            const newLocale = prevState.localeContext.locale === "id" ? "en" : "id";
+            localStorage.setItem("locale", newLocale);
+            return {
+              localeContext: {
+                ...prevState.localeContext,
+                locale : newLocale
+              },
+            };
+          });
+        },
+      },
     };
     this.onLoginSuccess = this.onLoginSuccess.bind(this);
     this.onLogout = this.onLogout.bind(this);
@@ -22,10 +38,10 @@ class ContactApp extends React.Component {
   onLogout() {
     this.setState(() => {
       return {
-        authedUser: null
-      }
+        authedUser: null,
+      };
     });
-    putAccessToken('');
+    putAccessToken("");
   }
 
   async onLoginSuccess({ accessToken }) {
@@ -43,7 +59,7 @@ class ContactApp extends React.Component {
     this.setState(() => {
       return {
         authedUser: data,
-        initializing: false
+        initializing: false,
       };
     });
   }
@@ -55,33 +71,49 @@ class ContactApp extends React.Component {
 
     if (this.state.authedUser === null) {
       return (
-        <div className="contact-app">
-          <header className="contact-app__header">
-            <h1>Aplikasi Kontak</h1>
-          </header>
-          <main>
-            <Routes>
-              <Route path="/*" element={<LoginPage loginSuccess={this.onLoginSuccess} />} />
-              <Route path="/register" element={<RegisterPage />} />
-            </Routes>
-          </main>
-        </div>
+        <LocaleProvider value={this.state.localeContext}>
+          <div className="contact-app">
+            <header className="contact-app__header">
+              <h1>
+                aplikasi kontak
+              </h1>
+            </header>
+            <main>
+              <Routes>
+                <Route
+                  path="/*"
+                  element={<LoginPage loginSuccess={this.onLoginSuccess} />}
+                />
+                <Route path="/register" element={<RegisterPage />} />
+              </Routes>
+            </main>
+          </div>
+        </LocaleProvider>
       );
     }
 
     return (
-      <div className="contact-app">
-        <header className="contact-app__header">
-          <h1>Aplikasi Kontak</h1>
-          <Navigation logout={this.onLogout} name={this.state.authedUser.name} />
-        </header>
-        <main>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/add" element={<AddPage />} />
-          </Routes>
-        </main>
-      </div>
+      <LocaleProvider value={this.state.localeContext}>
+        <div className="contact-app">
+          <header className="contact-app__header">
+            <h1>
+              {this.state.localeContext.locale === "en"
+                ? "Aplikasi Kontak"
+                : "Contact App"}
+            </h1>
+            <Navigation
+              logout={this.onLogout}
+              name={this.state.authedUser.name}
+            />
+          </header>
+          <main>
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/add" element={<AddPage />} />
+            </Routes>
+          </main>
+        </div>
+      </LocaleProvider>
     );
   }
 }
